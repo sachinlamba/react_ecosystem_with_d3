e89bf5a75734f5466c1b7764987c6e183c13fceb
Sachin Lamba <sachin.lamba@moldedbits.com>
2017-12-28 19:06:43 +0530

    Update Graph1.jsx

--- src/components/Graph1.jsx
+++ src/components/Graph1.jsx
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
-import { scaleLinear } from 'd3-scale';
-import { max } from 'd3-array';
-import { select } from 'd3-selection';
+import * as d3 from "d3";
+// import { scaleLinear } from 'd3-scale';
+// import { max } from 'd3-array';
+// import { select } from 'd3-selection';
 
 class Graph1 extends Component {
    constructor(props){
       super(props)
-      this.createGraph1 = this.createGraph1.bind(this)
+      // this.createGraph1 = this.createGraph1.bind(this)
    }
    componentDidMount() {
       this.createGraph1()
    }
-   componentDidUpdate() {
-      this.createGraph1()
-   }
+   // componentDidUpdate() {
+   //    this.createGraph1()
+   // }
    createGraph1() {
       // const node = this.node
-    const dataMax = max(this.props.data)
-    const yScale = scaleLinear()
+    const dataMax = d3.max(this.props.data)
+    const yScale = d3.scaleLinear()
        .domain([0, dataMax])
        .range([0, this.props.size[1]])
-   select("#" + this.props.graph)
+   d3.select("#" + this.props.graph)
       .selectAll('rect')
       .data(this.props.data)
       .enter()
       .append('rect')
 
-   select("#" + this.props.graph)
+   d3.select("#" + this.props.graph)
       .selectAll('rect')
       .data(this.props.data)
       .exit()
       .remove()
 
-   select("#" + this.props.graph)
+   d3.select("#" + this.props.graph)
       .selectAll('rect')
       .data(this.props.data)
       .style('fill', '#fe9922')

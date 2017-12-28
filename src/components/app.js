7d79ddf74fa06f6a580c2b68b92c71cf80fd2903
Sachin Lamba <sachin.lamba@moldedbits.com>
2017-12-28 19:06:26 +0530

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ErrorBoundary from './ErrorBoundary.jsx';
+import TtGraph from './TtGraph.jsx';
 import Graph1 from './Graph1.jsx';
 import '../styles/app.css';
 
@@ -7,15 +8,18 @@ export default function app() {
 	return (
 		<div>
 			<h1>JSX</h1>
-			<div>JSX span!!</div>
+			<div>JSX span!</div>
 			<ErrorBoundary>
-				<span>
-					<Graph1 graph="graph1" GraphNumber="1" data={[5,10,1,3]} size={[500,500]}></Graph1>
-				</span>
+					<TtGraph graph="graph3" GraphNumber="3" data={[15,10,56,23]} size={[500,500]}></TtGraph>
 			</ErrorBoundary>
 			<ErrorBoundary>
 				<span>
 					<Graph1 graph="graph2" GraphNumber="2" data={[15,10,56,23]} size={[500,500]}></Graph1>
+				</span>
+			</ErrorBoundary>
+			<ErrorBoundary>
+				<span>
+					<Graph1 graph="graph1" GraphNumber="1" data={[5,10,1,3]} size={[500,500]}></Graph1>
 				</span>
 			</ErrorBoundary>
 		</div>
